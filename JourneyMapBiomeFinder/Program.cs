26d21c9c7cf4ba97d4f52561078cd0e331e170ad
Brycey92 <bbrowner@optonline.net>
2023-06-10 04:37:51 -0400

    Add block finding functionality

--- JourneyMapBiomeFinder/Program.cs
+++ JourneyMapBiomeFinder/Program.cs
@@ -1,55 +1,88 @@
 ﻿using Substrate;
 using Substrate.Core;
 using Substrate.Nbt;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using System.Xml.Linq;
 
 namespace JourneyMapBiomeFinder
 {
     class Program
     {
+        private static string blockTagNameStr = "block";
+        private static string blockNameStr = "Name";
         private static string biomeNameStr = "biome_name";
+
+        private enum SearchType
+        {
+            BIOME_SEARCH,
+            BLOCK_SEARCH
+        }
+
+        private enum ReturnAction
+        {
+            NO_ACTION,
+            CONTINUE_ACTION,
+            ERROR_ACTION
+        }
 
         private static void Main(string[] args)
         {
-            if (args.Length < 2 || args[0] == null || args[0].Trim().Equals("") || args[1] == null || args[1].Trim().Equals(""))
+            if (args.Length < 3 ||
+                args[0] == null || args[0].Trim().Equals("") ||
+                args[1] == null || args[1].Trim().Equals("") ||
+                args[2] == null || args[2].Trim().Equals(""))
             {
-                Console.WriteLine("Usage: <biome_name> <path>");
-                Console.WriteLine("\tbiome_name:\te.g. \"minecraft:plains\"");
-                Console.WriteLine("\tpath:\tPath to your Minecraft instance's journeymap/data/mp/YourServerName folder");
-                Console.WriteLine("\t\tThe server name folder may have a long string of letters, numbers, and \'~\' after the name");
+                PrintUsage();
                 return;
             }
 
-            if (!Directory.Exists(args[1]))
+            SearchType searchType;
+            if (args[0].Equals("--biome"))
             {
-                Console.WriteLine(args[1] + " does not exist or is not a folder, aborting");
+                searchType = SearchType.BIOME_SEARCH;
+            }
+            else if (args[0].Equals("--block"))
+            {
+                searchType = SearchType.BLOCK_SEARCH;
+            }
+            else
+            {
+                Console.WriteLine("Unrecognized argument \"" + args[0] + "\", aborting\n");
+                PrintUsage();
                 return;
             }
 
-            Console.WriteLine("Searching for biomes...");
+            if (!Directory.Exists(args[2]))
+            {
+                Console.WriteLine(args[2] + " does not exist or is not a folder, aborting");
+                return;
+            }
 
-            string[] files = Directory.GetFiles(args[1], "*", SearchOption.AllDirectories);
+            Console.WriteLine("Searching for " + args[0].Trim('-') + "s...");
+
+            string[] files = Directory.GetFiles(args[2], "*", SearchOption.AllDirectories);
             foreach (string path in files)
             {
-                if (FindBiomesInFile(path, args[0]) != 0)
+                if (FindInFile(path, args[1], searchType) != 0)
                 {
                     return;
                 }
             }
 
-            Console.WriteLine("Finished searching for biomes");
+            Console.WriteLine("Finished searching for " + args[0].Trim('-') + "s");
         }
 
-        private static int FindBiomesInFile(string path, string biomeToFind)
+        private static int FindInFile(string path, string stringToFind, SearchType searchType)
         {
-            if(!Path.GetExtension(path).ToLower().Equals(".mca"))// || !Path.GetFileName(path).StartsWith('r'))
+            if (!Path.GetExtension(path).ToLower().Equals(".mca"))// || !Path.GetFileName(path).StartsWith('r'))
             {
                 return 0;
             }
-            
+
             RegionFile regionFile = TryCreateFrom(path);
 
-            if(regionFile == null)
+            if (regionFile == null)
             {
                 Console.WriteLine(path + " is not a valid region file, aborting");
                 return -1;
@@ -63,7 +96,16 @@ namespace JourneyMapBiomeFinder
                     if (regionFile.HasChunk(x, z))
                     {
                         NbtTree chunkTree = new NbtTree();
-                        chunkTree.ReadFrom(regionFile.GetChunkDataInputStream(x, z));
+                        try
+                        {
+                            chunkTree.ReadFrom(regionFile.GetChunkDataInputStream(x, z));
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("\nIn " + path + " chunk " + x + "," + z + ":");
+                            Console.WriteLine(e.StackTrace + '\n');
+                            continue;
+                        }
 
                         if (chunkTree.Root == null)
                         {
@@ -84,36 +126,119 @@ namespace JourneyMapBiomeFinder
 
                             TagNodeCompound blockCompound = blockNode.ToTagCompound();
 
-                            if(!blockCompound.ContainsKey(biomeNameStr))
+                            ReturnAction returnAction;
+                            switch (searchType)
                             {
-                                continue;
+                                case SearchType.BIOME_SEARCH:
+                                    returnAction = FindBiomeInBlockNode(blockCompound, stringToFind, path, x, z, key);
+                                    break;
+                                case SearchType.BLOCK_SEARCH:
+                                    returnAction = FindBlockInBlockNode(blockCompound, stringToFind, path, x, z, key);
+                                    break;
+                                default:
+                                    returnAction = ReturnAction.ERROR_ACTION;
+                                    break;
                             }
 
-                            TagNode biomeNameNode = blockCompound[biomeNameStr];
-
-                            if (biomeNameNode == null)
+                            switch (returnAction)
                             {
-                                Console.WriteLine(path + " chunk " + x + "," + z + " block (X,Z) " + key + " \"biome_name\" value is null, aborting");
-                                return -1;
+                                case ReturnAction.NO_ACTION:
+                                    break;
+                                case ReturnAction.CONTINUE_ACTION:
+                                    continue;
+                                case ReturnAction.ERROR_ACTION:
+                                default:
+                                    return -1;
                             }
-
-                            if (!biomeNameNode.IsCastableTo(TagType.TAG_STRING))
-                            {
-                                Console.WriteLine(path + " chunk " + x + "," + z + " block (X,Z) " + key + " \"biome_name\" value is not castable to string, aborting");
-                                return -1;
-                            }
-
-                            if (!biomeNameNode.ToString().Equals(biomeToFind))
-                            {
-                                continue;
-                            }
-
-                            Console.WriteLine("Found " + biomeToFind + " at (X,Z) " + key);
                         }
                     }
                 }
             }
             return 0;
+        }
+
+        private static ReturnAction FindBiomeInBlockNode([NotNull] TagNodeCompound blockCompound, string biomeToFind, string path, int x, int z, string key)
+        {
+            // Each block has a string called "biome_name"
+            if (!blockCompound.ContainsKey(biomeNameStr))
+            {
+                return ReturnAction.CONTINUE_ACTION;
+            }
+
+            TagNode biomeNameNode = blockCompound[biomeNameStr];
+
+            if (biomeNameNode == null)
+            {
+                Console.WriteLine(path + " chunk " + x + "," + z + " block (X,Z) " + key + " \"" + biomeNameStr + "\" value is null, aborting");
+                return ReturnAction.ERROR_ACTION;
+            }
+
+            if (!biomeNameNode.IsCastableTo(TagType.TAG_STRING))
+            {
+                Console.WriteLine(path + " chunk " + x + "," + z + " block (X,Z) " + key + " \"" + biomeNameStr + "\" value is not castable to string, aborting");
+                return ReturnAction.ERROR_ACTION;
+            }
+
+            if (!biomeToFind.Equals(biomeNameNode.ToString()))
+            {
+                return ReturnAction.CONTINUE_ACTION;
+            }
+
+            Console.WriteLine("Found " + biomeToFind + " at (X,Z) " + key);
+            return ReturnAction.NO_ACTION;
+        }
+
+        private static ReturnAction FindBlockInBlockNode([NotNull] TagNodeCompound blockCompound, string blockToFind, string path, int x, int z, string key)
+        {
+            // Each block has a compound tag called "block"
+            if (!blockCompound.ContainsKey(blockTagNameStr))
+            {
+                return ReturnAction.CONTINUE_ACTION;
+            }
+
+            TagNode blockBlockNode = blockCompound[blockTagNameStr];
+
+            if (blockBlockNode == null)
+            {
+                Console.WriteLine(path + " chunk " + x + "," + z + " block (X,Z) " + key + " \"" + blockTagNameStr + "\" value is null, aborting");
+                return ReturnAction.ERROR_ACTION;
+            }
+
+            if (!blockBlockNode.IsCastableTo(TagType.TAG_COMPOUND))
+            {
+                Console.WriteLine(path + " chunk " + x + "," + z + " block (X,Z) " + key + " \"" + blockTagNameStr + "\" value is not castable to TagNodeCompound, aborting");
+                return ReturnAction.ERROR_ACTION;
+            }
+
+            TagNodeCompound blockBlockCompound = blockBlockNode.ToTagCompound();
+
+            // Each "block" compound tag has a string called "Name"
+            if (!blockBlockCompound.ContainsKey(blockNameStr))
+            {
+                return ReturnAction.CONTINUE_ACTION;
+            }
+
+            TagNode blockNameNode = blockBlockCompound[blockNameStr];
+
+            if (blockNameNode == null)
+            {
+                Console.WriteLine(path + " chunk " + x + "," + z + " block (X,Z) " + key + " \"" + blockNameStr + "\" value is null, aborting");
+                return ReturnAction.ERROR_ACTION;
+            }
+
+            if (!blockNameNode.IsCastableTo(TagType.TAG_STRING))
+            {
+                Console.WriteLine(path + " chunk " + x + "," + z + " block (X,Z) " + key + " \"" + blockNameStr + "\" value is not castable to string, aborting");
+                return ReturnAction.ERROR_ACTION;
+            }
+
+            if (!blockToFind.Equals(blockNameNode.ToString()))
+            {
+                return ReturnAction.CONTINUE_ACTION;
+            }
+
+            Console.WriteLine("Found " + blockToFind + " at (X,Z) " + key);
+            return ReturnAction.NO_ACTION;
         }
 
         private static RegionFile? TryCreateFrom(string path)
@@ -127,6 +252,17 @@ namespace JourneyMapBiomeFinder
                 Console.WriteLine(ex.ToString());
                 return null;
             }
+        }
+
+        private static void PrintUsage()
+        {
+            Console.WriteLine("Usage: [--biome <biome_name>]");
+            Console.WriteLine("     | [--block <block_name>]");
+            Console.WriteLine("       <path>");
+            Console.WriteLine("\tbiome_name:\te.g. \"minecraft:plains\"");
+            Console.WriteLine("\tblock_name:\re.g. \"minecraft:iron_block\"");
+            Console.WriteLine("\tpath:\tPath to your Minecraft instance's journeymap/data/mp/YourServerName folder");
+            Console.WriteLine("\t\tThe server name folder may have a long string of letters, numbers, and \'~\' characters after the name");
         }
     }
 }
